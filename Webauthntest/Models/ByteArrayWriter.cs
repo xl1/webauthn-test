5378aa6eb58bb5f6142193980650c2c6014a8d1e
xl1 <shiba200712@gmail.com>
2018-10-01 01:18:52 +0900

    ByteArrayWriter

---
+++ Webauthntest/Models/ByteArrayWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Webauthntest.Models
+{
+    public ref struct ByteArrayWriter
+    {
+        private Span<byte> _span;
+        private int _index;
+
+        public ByteArrayWriter(int length)
+        {
+            _span = new byte[length];
+            _index = 0;
+        }
+
+        public ByteArrayWriter Set(byte b)
+        {
+            _span[_index] = b;
+            _index++;
+            return this;
+        }
+
+        public ByteArrayWriter CopyFrom(ReadOnlySpan<byte> src)
+        {
+            src.CopyTo(_span.Slice(_index));
+            _index += src.Length;
+            return this;
+        }
+
+        public byte[] ToArray() => _span.ToArray();
+    }
+}
