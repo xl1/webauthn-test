9e9acefbbb6886f37cf8bbec53f9a089a4542805
xl1 <shiba200712@gmail.com>
2018-10-01 01:24:19 +0900

    Implementing verification for "fido-u2f" attestation format

---
+++ Webauthntest/Models/FIDOU2FAttestationStatement.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using PeterO.Cbor;
+
+namespace Webauthntest.Models
+{
+    public class FIDOU2FAttestationStatement
+    {
+        public byte[][] X509Cert { get; set; }
+        public byte[] Signature { get; set; }
+
+        public static FIDOU2FAttestationStatement Decode(CBORObject obj)
+        {
+            var x5c = obj["x5c"];
+            var cert = Enumerable.Range(0, x5c.Count).Select(i => x5c[i]?.GetByteString());
+            return new FIDOU2FAttestationStatement
+            {
+                X509Cert = cert.ToArray(),
+                Signature = obj["sig"]?.GetByteString(),
+            };
+        }
+
+        public bool VerifyData(byte[] data)
+        {
+            if (X509Cert == null || X509Cert.Length != 1)
+                return false;
+
+            if (Signature == null)
+                return false;
+
+            var rawSignature = DERSignature.Deserialize(Signature);
+            var ecDsa = new X509Certificate2(X509Cert[0]).GetECDsaPublicKey();
+            return ecDsa.VerifyData(data, rawSignature, HashAlgorithmName.SHA256);
+        }
+    }
+}
